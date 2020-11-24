93357616e43f6afc4fd87b3a3bb3ac54d3f87698
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2020-11-24 19:34:05 +0100

    Using docker-compose

---
+++ 01.dive-into-docker/simple-with-redis/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const redis = require('redis');
+const app = express();
+const process = require('process');
+
+const client = redis.createClient({
+  host: 'redis-server',
+  port: '6379',
+});
+client.on('error', function (error) {
+  console.error(error);
+});
+client.set('visits', 0);
+
+app.get('/', (req, res) => {
+  process.exit(0);
+  client.get('visits', (err, visits) => {
+    res.send('Number of visits is ' + visits);
+    client.set('visits', parseInt(visits) + 1);
+  });
+});
+
+const port = 8080;
+app.listen(port, () => {
+  console.log('App Redis is running on port 8080');
+});
