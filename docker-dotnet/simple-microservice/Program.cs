10dd10bf33e2a544ce1db434c349d3d19f8b6d5b
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2021-01-06 11:40:44 +0100

    simple-microservice: Update Docker

--- docker-dotnet/simple-microservice/Program.cs
+++ docker-dotnet/simple-microservice/Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
@@ -7,20 +8,30 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace SimpleMicroservice
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+namespace SimpleMicroservice {
+  public class Program {
+    public static void Main (string[] args) {
+      CreateHostBuilder (args).Build ().Run ();
     }
+
+    public static IHostBuilder CreateHostBuilder (string[] args) {
+      return Host.CreateDefaultBuilder (args)
+        .ConfigureWebHostDefaults ((webBuilder) => {
+          webBuilder.UseKestrel ();
+          webBuilder.UseContentRoot (Directory.GetCurrentDirectory ());
+          webBuilder.UseUrls ($"http://*:{HostPort}");
+          webBuilder.UseIISIntegration ();
+
+          webBuilder.UseStartup<Startup> ();
+        });
+    }
+
+    private static bool IsDevelopment =>
+      Environment.GetEnvironmentVariable ("ASPNETCORE_ENVIRONMENT") == "Development";
+
+    public static string HostPort =>
+      IsDevelopment ?
+      "5000" :
+      Environment.GetEnvironmentVariable ("PORT");
+  }
 }
